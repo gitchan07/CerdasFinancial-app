097beb44efd936e62a4b34d3c28d791ce77494a1
Muhammad Quraisy <quraisy@uai.ac.id>
2024-11-26 09:25:32 +0700

    configure code formatter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 function App() {
-  return (
-    <>
-      <div className="h-screen flex justify-between items-center">
-        <h1 className="text-center font-semibold">Cerdas Financial</h1>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="flex h-screen items-center justify-between">
+                <h1 className="text-center font-semibold">Cerdas Financial</h1>
+                <p className="text-sm leading-3">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolor, et deleniti porro autem reiciendis qui, sed debitis
+                    magni quisquam officia eum explicabo labore sunt. Culpa quas
+                    placeat veritatis beatae esse!
+                </p>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;

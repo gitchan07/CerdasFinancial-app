c318e3b47beb350f37b965fd4611ff184801aa30
Muhammad Quraisy <quraisy@uai.ac.id>
2024-11-26 09:34:55 +0700

    configure react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TestPage from "./pages/TestPage";
+
 function App() {
     return (
-        <>
-            <div className="flex h-screen items-center justify-between">
-                <h1 className="text-center font-semibold">Cerdas Financial</h1>
-                <p className="text-sm leading-3">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Dolor, et deleniti porro autem reiciendis qui, sed debitis
-                    magni quisquam officia eum explicabo labore sunt. Culpa quas
-                    placeat veritatis beatae esse!
-                </p>
-            </div>
-        </>
+        <BrowserRouter>
+            <Routes>
+                {/* For initiating react router dom This page can be deleted and replaced*/}
+                <Route index element={<TestPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 

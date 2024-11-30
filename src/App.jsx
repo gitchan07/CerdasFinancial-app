d257db4d13be2f902de93c46f33c9a0be214312f
Muhammad Quraisy <58893112+iyefreedy@users.noreply.github.com>
2024-11-30 13:33:16 +0700

    Revert "Login page"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-// import React from 'react';
-import React from 'react';
-import Login from './pages/Login/index';
-import './index.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TestPage from "./pages/TestPage";
 
 function App() {
-  return (
-    <div className="App"> 
-      <Login />
-    </div> 
-  ); 
-} 
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* For initiating react router dom This page can be deleted and replaced*/}
+                <Route index element={<TestPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
 export default App;
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,15 +1,15 @@
-// export default function TestPage() {
-//     return (
-//         <div className="flex h-screen items-center justify-between">
-//             <div className="container mx-auto text-center">
-//                 <h1 className="font-semibold">Cerdas Financial</h1>
-//                 <p className="text-sm leading-3">
-//                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-//                     Dolor, et deleniti porro autem reiciendis qui, sed debitis
-//                     magni quisquam officia eum explicabo labore sunt. Culpa quas
-//                     placeat veritatis beatae esse!
-//                 </p>
-//             </div>
-//         </div>
-//     );
-// }
+export default function TestPage() {
+    return (
+        <div className="flex h-screen items-center justify-between">
+            <div className="container mx-auto text-center">
+                <h1 className="font-semibold">Cerdas Financial</h1>
+                <p className="text-sm leading-3">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolor, et deleniti porro autem reiciendis qui, sed debitis
+                    magni quisquam officia eum explicabo labore sunt. Culpa quas
+                    placeat veritatis beatae esse!
+                </p>
+            </div>
+        </div>
+    );
+}

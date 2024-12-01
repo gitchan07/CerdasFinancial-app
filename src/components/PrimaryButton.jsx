604457e1b16a3576ea750223b80019d2b5489bf1
Muhammad Quraisy <iye.fredickson@gmail.com>
2024-12-01 13:13:32 +0700

    theme: create primary button component

---
+++ src/components/PrimaryButton.jsx
@@ -0,0 +1,25 @@
+import { forwardRef } from "react";
+import { Link } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
+
+const PrimaryButton = forwardRef((props, ref) => {
+    const classNames = twMerge(
+        "rounded bg-blue-600 px-2 py-1 text-gray-50 font-medium transition-colors duration-200 hover:bg-blue-900 hover:text-white",
+        props.className
+    );
+
+    if (props.as === "link") {
+        return <Link ref={ref} {...props} className={classNames} />;
+    } else {
+        return (
+            <button
+                ref={ref}
+                {...props}
+                type={props.type || "button"}
+                className={classNames}
+            />
+        );
+    }
+});
+
+export default PrimaryButton;

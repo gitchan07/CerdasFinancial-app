f2733924fdf61e2adade7e3d514ac2c4c624d6b3
Muhammad Quraisy <iye.fredickson@gmail.com>
2024-12-02 22:19:40 +0700

    theme: add padding to primary button

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 
 const PrimaryButton = forwardRef((props, ref) => {
     const classNames = twMerge(
-        "rounded bg-blue-600 px-2 py-1 text-gray-50 font-medium transition-colors duration-200 hover:bg-blue-900 hover:text-white",
+        "rounded bg-blue-600 px-4 py-2 text-gray-50 font-medium transition-colors duration-200 hover:bg-blue-900 hover:text-white",
         props.className
     );
 

cdfa3fc7ed2d31702abda91d87a72e5f13802a4b
Muhammad Quraisy <iye.fredickson@gmail.com>
2024-12-02 22:48:10 +0700

    fix: resolve import

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // import React from 'react';
 import React from "react";
-import Login from "./pages/Login/index";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route index element={<LandingPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
-                <Route path="*" element={<LoginPage />} />
             </Routes>
         </BrowserRouter>
     );

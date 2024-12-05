b87bc236c314a06bd953708bfb9f99e7c2437d34
kholikhub <kholik.work@gmail.com>
2024-12-05 22:37:04 +0700

    edit: card amd home ux

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,17 +8,8 @@ const Card = ({ name, description, progress, total, imageUrl }) => {
         alt={name}
         className="w-full h-40 object-cover rounded-md mb-4"
       />
-      <h2 className="text-lg font-bold text-gray-800 mb-2">{name}</h2>
-      <p className="text-gray-600 text-sm mb-4 flex-grow">{description}</p>
-      <div className="flex items-center w-full mt-auto">
-        <div className="relative w-full h-6 bg-gray-200 rounded-full mr-2">
-          <div
-            className="absolute top-0 left-0 h-6 bg-blue-500 rounded-full"
-            style={{ width: `${(progress / total) * 100}%` }}
-          ></div>
-        </div>
-        <span className="text-sm text-gray-500">{`${progress}/${total}`}</span>
-      </div>
+      <h2 className="text-lg font-bold mb-2">{name}</h2>
+      <p className="text-neutral-600 text-sm mb-4 flex-grow">{description}</p>
     </div>
   );
 };

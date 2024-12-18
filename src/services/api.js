eeb7d23d54d2a5b4d53e9a2cc1344b1f809972e5
Muhammad Quraisy <quraisy@uai.ac.id>
2024-12-18 13:01:04 +0700

    feat(api/auth): implement login and registration api communication

--- src/services/api.js
+++ src/services/api.js
@@ -1,29 +1,47 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: "http://117.53.47.31:5000/", // Base URL API
-// });
+const api = axios.create({
+    baseURL: "https://api-cerdasfinancial.crowintheglass.com/api/v1", // Base URL API
+});
 
+export const login = async (formData) => {
+    const response = await api.post("/login", formData);
+    return response;
+};
+
+export const register = async (formData) => {
+    const response = await api.post("/register", formData);
+    return response;
+};
+
+export const getCurrentUser = async (token) => {
+    const response = await api.get("/me", {
+        headers: {
+            Authorization: "Bearer " + token,
+        },
+    });
+    return response;
+};
 // Fetch courses data
 export const fetchCourses = async () => {
-  try {
-    console.log("Fetching courses...");
-    const response = await axios.get("/api/courses");
-    console.log("Response:", response);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching courses:", error);
-    throw error;
-  }
+    try {
+        console.log("Fetching courses...");
+        const response = await api.get("/api/courses");
+        console.log("Response:", response);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching courses:", error);
+        throw error;
+    }
 };
 
 // Fetch user profile data
 export const fetchUserProfile = async () => {
-  try {
-    const response = await axios.get("api/users"); // Endpoint disesuaikan
-    return response.data; // Pastikan struktur data sesuai dengan response API
-  } catch (error) {
-    console.error("Error fetching user profile:", error);
-    throw error;
-  }
+    try {
+        const response = await api.get("api/users"); // Endpoint disesuaikan
+        return response.data; // Pastikan struktur data sesuai dengan response API
+    } catch (error) {
+        console.error("Error fetching user profile:", error);
+        throw error;
+    }
 };
